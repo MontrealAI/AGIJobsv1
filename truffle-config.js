1aeaba629c67ff19c699fae9d3643636d4991385
MONTREAL.AI <info@montreal.ai>
2025-09-20 10:52:54 -0400

    chore: capture gas report in CI

--- truffle-config.js
+++ truffle-config.js
@@ -3,6 +3,19 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 const { MNEMONIC, RPC_MAINNET, RPC_SEPOLIA, ETHERSCAN_API_KEY } = process.env;
 
+const mocha = { timeout: 600000 };
+
+if (process.env.REPORT_GAS === 'true') {
+  mocha.reporter = 'eth-gas-reporter';
+  mocha.reporterOptions = {
+    currency: 'USD',
+    gasPrice: 30,
+    noColors: true,
+    showTimeSpent: true,
+    excludeContracts: ['Migrations'],
+  };
+}
+
 module.exports = {
   plugins: ['truffle-plugin-verify', 'truffle-hardhat-coverage'],
   api_keys: { etherscan: ETHERSCAN_API_KEY },
@@ -29,5 +42,5 @@ module.exports = {
       settings: { optimizer: { enabled: true, runs: 600 } }
     }
   },
-  mocha: { timeout: 600000 }
+  mocha
 };
